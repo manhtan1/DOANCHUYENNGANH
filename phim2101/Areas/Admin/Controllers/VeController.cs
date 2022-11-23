536aa7d2377dbd3355879909d0135295c0d1f17d
as <as@LAPTOP-N0OPM35J>
2022-11-23 20:23:26 +0700

    vé phim

---
+++ phim2101/Areas/Admin/Controllers/VeController.cs
@@ -0,0 +1,125 @@
+﻿using phim2101.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace phim2101.Areas.Admin.Controllers
+{
+    public class VeController : Controller
+    {
+        private DBContext db = new DBContext();
+        // GET: Admin/Ve
+        public ActionResult Index()
+        {
+            return View(db.Ves.ToList());
+        }
+
+        // GET: Admin/Ves/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ve ve = db.Ves.Find(id);
+            if (ve == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ve);
+        }
+
+        // GET: Admin/Ves/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Ves/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaVe,MaPhong,MaPhim")] Ve ve)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ves.Add(ve);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ve);
+        }
+
+        // GET: Admin/Ves/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ve ve = db.Ves.Find(id);
+            if (ve == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ve);
+        }
+
+        // POST: Admin/Ves/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaVe, MaPhong, MaPhim")] Ve ve)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ve).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ve);
+        }
+
+        // GET: Admin/Ves/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ve ve = db.Ves.Find(id);
+            if (ve == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ve);
+        }
+
+        // POST: Admin/Ves/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ve ve = db.Ves.Find(id);
+            db.Ves.Remove(ve);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
