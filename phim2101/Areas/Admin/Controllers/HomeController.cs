ff23a6fbb5c3df3490e263bdf8cfd5dbaccf56c7
mạnh tân <manhtanqhp@gmail.com>
2022-12-02 16:17:04 +0700

    phân quyền (chưa xong)

--- phim2101/Areas/Admin/Controllers/HomeController.cs
+++ phim2101/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using phim2101.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,33 @@ namespace phim2101.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private DBContext db = new DBContext();
         // GET: Admin/Home
+
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(string user, string pw)
+        {
+            var nhanvien = db.NhanViens.SingleOrDefault(m => m.UserName.ToLower() == user.ToLower() && m.Password == pw);
+            if (nhanvien !=null)
+            {
+                Session["user"] = user;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng");
+                return View();
+            }
+            
+
         }
     }
 }
